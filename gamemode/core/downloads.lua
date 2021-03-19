352f67ed1c307854b7d6d7fc777daed30b57d91b
Alex <16008982+Valafi@users.noreply.github.com>
2021-03-19 09:12:16 -0700

    Update downloads.lua

--- gamemode/core/downloads.lua
+++ gamemode/core/downloads.lua
@@ -2,13 +2,20 @@
 --
 -- @Author: Garrus2142
 -- @Date:   2017-07-25 16:15:46
--- @Last Modified by:   Garrus2142
--- @Last Modified time: 2017-07-26 14:46:14
+-- @Last Modified by:   Valafi
+-- @Last Modified time: 2021-03-19 09:10:55
 
-resource.AddWorkshop("787790694") -- slash_highschool
+-- Main Content
+resource.AddWorkshop("788009163") -- Utopia Games - Slashers content 1
+resource.AddWorkshop("857992903") -- Utopia Games - Slashers content 2
+
+-- Season 1 Maps
 resource.AddWorkshop("787938932") -- slash_summercamp
 resource.AddWorkshop("798554357") -- slash_selvage
 resource.AddWorkshop("858049276") -- slash_subway
-resource.AddWorkshop("1091061063") -- slash_logde
-resource.AddWorkshop("788009163") -- Slashers content 1
-resource.AddWorkshop("857992903") -- Slashers content 2
+resource.AddWorkshop("787790694") -- slash_highschool
+resource.AddWorkshop("1091061063") -- slash_lodge
+
+-- Season 2 Maps
+resource.AddWorkshop("1247351614") -- Slashers Season 2 Map Content
+resource.AddWorkshop("1247285424") -- slash_motel
