08cd46675c38650a83c1272c45257002b786e804
Alex <bleedingoptimism@gmail.com>
2021-03-24 13:43:22 -0700

    Fix anims part 1

--- gamemodes/slashers/gamemode/shared.lua
+++ gamemodes/slashers/gamemode/shared.lua
@@ -35,3 +35,45 @@ print("Version: " .. GM.Version)
 print("Workshop: " .. GM.Workshop)
 print("Github: " .. GM.Github)
 print("\n###                 Thanks for playing                ###\n")
+
+-- TODO: Should probably move this stuff, and should try to find a way to detect ACTs (try examining an addon that plays anims)
+local mdl_land_list = {}
+-- Survivors
+mdl_land_list["models/player/eli.mdl"] = true -- Fallback survivor model
+mdl_land_list["models/steinman/slashers/sport_pm.mdl"] = true
+mdl_land_list["models/steinman/slashers/popular_pm.mdl"] = false
+mdl_land_list["models/steinman/slashers/nerd_pm.mdl"] = false
+mdl_land_list["models/steinman/slashers/fat_pm.mdl"] = false
+mdl_land_list["models/player/korka007/maxc.mdl"] = false
+mdl_land_list["models/steinman/slashers/junky_pm.mdl"] = false
+mdl_land_list["models/steinman/slashers/emo_pm.mdl"] = true
+mdl_land_list["models/player/spike/lamar.mdl"] = true
+mdl_land_list["models/steinman/slashers/sheriff_pm.mdl"] = true
+-- Killers
+mdl_land_list["models/player/screamplayermodel/scream/scream.mdl"] = true -- Old slash_highschool killer
+mdl_land_list["models/error/ghostface/classic_ghostface_pm.mdl"] = true -- New slash_highschool killer
+mdl_land_list["models/steinman/slashers/intruder_pm.mdl"] = false -- Old slash_lodge killer
+mdl_land_list["models/players/an_shining_jack.mdl"] = true -- New slash_lodge killer
+mdl_land_list["models/steinman/slashers/bates_pm.mdl"] = false -- slash_motel killer
+mdl_land_list["models/player/dewobedil/mike_myers/default_p.mdl"] = true -- Old slash_selvage killer
+mdl_land_list["models/players/mj_dbd_myers.mdl"] = false -- New slash_selvage killer
+mdl_land_list["models/slender_arrival/chaser.mdl"] = false -- Old slash_subway killer, appears to have one, but looks fucked up
+mdl_land_list["models/player/freddykrueger.mdl"] = true -- New slash_subway killer
+mdl_land_list["models/player/mkx_jason.mdl"] = false -- slash_summercamp killer, appears to have one, but looks fucked up
+function GM:HandlePlayerLanding( ply, velocity, WasOnGround )
+
+	if ( ply:GetMoveType() == MOVETYPE_NOCLIP ) then return end
+
+	if ( ply:IsOnGround() && !WasOnGround ) then
+        if mdl_land_list[ply:GetModel()] then -- When in doubt, leave it out
+		    ply:AnimRestartGesture( GESTURE_SLOT_JUMP, ACT_LAND, true )
+        end
+	end
+
+end
+-- TODO: Stop glitch (possibly from prediction?)
+--[[hook.Add( "PlayerInitialSpawn", "StopCrouchFuckery", function( ply ) 
+    
+    ply:SetHullDuck(Vector(-16, -16, 0), Vector(16, 16, 46)) -- TODO: Get better number than 46
+    
+end )]]
